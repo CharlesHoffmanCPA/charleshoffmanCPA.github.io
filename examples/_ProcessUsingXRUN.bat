5784fbeafcc94cf459f451f2b86030f4eebc02c3
Charles Hoffman, CPA <charles.hoffman@me.com>
2018-09-20 22:25:08 +0000

    Delete _ProcessUsingXRUN.bat

--- examples/_ProcessUsingXRUN.bat
+++
@@ -1,4 +0,0 @@
-Rem This batch file starts XRUN using the specified XRUN input file.
-
-cd %~dp0
-java -Xmx1024m -jar c:\Programs\coyote\lib\xrun.jar _ProcessUsingXRUN.xrun >_ProcessUsingXRUN_Log.txt 2>&1
